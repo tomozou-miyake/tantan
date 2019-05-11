120543cf4f2dee1fc4db84763f1bd910b0f08971
tomozou-miyake <tomoki2802@gmail.com>
2019-05-11 23:58:03 +0900

    Edit page

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   root 'tweets#index'
   resources :tweets, only: [:index ,:new]
   resources :users, only: [:edit, :update]
+  resources :groups, only: [:new, :edit]
 end
